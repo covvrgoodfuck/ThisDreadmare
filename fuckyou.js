42ec69c5c998f78a13b2a6143cceef2c11d7b0ca
covvrgoodfuck <covvrgood@gmail.com>
2026-09-25 17:25:09 +0800

    Update fuckyou.js

--- fuckyou.js
+++ fuckyou.js
@@ -2,6 +2,7 @@
   "tokens": [
     "8981296384:AAFQxm4kOtXqbGIZZRdIyfY6z4adWQ29emg",
     "8687264645:AAF2y9xi8CKKSi9sxQCcUjfqBlHfSw9tBeM",
-    "8652563807:AAF7ZcwnArXSHQWcnUW3iFWfVdukTpHNnx8"
+    "8652563807:AAF7ZcwnArXSHQWcnUW3iFWfVdukTpHNnx8",
+    "8845698571:AAHWQQe-ajw3GRcRe-G63kxgqy2ata1Is_c,
   ]
 }
